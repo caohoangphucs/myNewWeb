3b8658289bad91f6b1bb637d99ca218e5d01f641
phucdeptrai <you@example.com>
2025-02-22 01:16:44 +0700

    do some stuff

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.body.style.transition = "background-color 1s";
-
-function changeColor() {
-    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = randomColor;
-}
-//fuck me all
-setInterval(changeColor, 1000);
+document.getElementById("title").innerHTML = "Chào mừng đến với JavaScript!";
